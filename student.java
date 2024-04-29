cb18f391dd4d6a5189519e6e0b19613fa7569a84
ANUSHA HY <141933075+AnushaHY@users.noreply.github.com>
2024-04-29 14:50:18 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,28 @@
+package studentMarks;
+
+
+class Student {
+    private String name;
+    private int[] marks;
+
+    public Student(String name, int[] marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getMarks() {
+        return marks;
+    }
+
+    public int getTotalMarks() {
+        int total = 0;
+        for (int mark : marks) {
+            total += mark;
+        }
+        return total;
+    }
+}
